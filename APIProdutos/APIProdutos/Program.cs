f255567e803c6c8a453bb0d435239a46ffad9682
Renato Groffe <renato.groffe@yahoo.com.br>
2018-10-10 20:03:17 -0300

    ASP.NET Core 2.1 + JWT (JSON Web Token) + Identity Core + Entity Framework Core com SQL Server + Entity Framework Core InMemory + Azure Key Vault
    ASP.NET Core 2.1 + JWT (JSON Web Token) + Identity Core + Entity Framework Core com SQL Server + Entity Framework Core InMemory + Azure Key Vault

---
+++ APIProdutos/APIProdutos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace APIProdutos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var builtConfig = config.Build();
+
+                    config.AddAzureKeyVault(
+                        builtConfig["AzureKeyVault:DNS"],
+                        builtConfig["AzureKeyVault:ClientId"],
+                        builtConfig["AzureKeyVault:ClientSecret"]);
+                })
+                .UseStartup<Startup>();
+    }
+}
---
+++ Scripts/Script-Produtos.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE dbo.Produtos(
+	CodigoBarras VARCHAR(13) NOT NULL,
+	Nome varchar(50) NOT NULL,
+	Preco numeric(15, 2) NOT NULL,
+	CONSTRAINT PK_Produtos PRIMARY KEY (CodigoBarras)
+)
+GO
